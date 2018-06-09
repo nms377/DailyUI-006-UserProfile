1fda8019392b33b2cbd99edd4226455dc5aeb392
Nicole Sandry <nms377@nyu.edu>
2018-06-08 16:25:04 -1000

    Created an element using DOM and created an event listener to change the heart icons to indicate that a user favorited a dog's profile.

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function favorite() {
+	var redHeart = document.createElement('p');
+	redHeart.id = "redHeart";
+	redHeart.innerHTML = "&#9829";
+	redHeart.style.display = "none";
+	redHeart.style.color = "rgba(255, 0, 0, 0.60";
+	fave.appendChild(redHeart);
+	redHeart.addEventListener("click", function() {
+		if (redHeart.style.display = "block") {
+			redHeart.style.display = "none";
+			whiteHeart.style.display = "block";
+		} else {
+			redHeart.style.display = "block";
+		}
+	});
+
+	var whiteHeart = document.getElementById('heart');
+	whiteHeart.addEventListener("click", function() {
+		if (whiteHeart.style.display = "block") {
+			whiteHeart.style.display = "none";
+			redHeart.style.display = "block";
+		} else {
+			whiteHeart.stle.display = "block";
+		}
+	});
+
+
+}
+
+favorite();
